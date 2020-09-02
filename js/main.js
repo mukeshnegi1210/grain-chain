0ab523be40b61d5bc1f7903274768925e7659a7c
mukeshnegi1210 <33194021+mukeshnegi1210@users.noreply.github.com>
2020-09-02 21:03:48 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,19 +8,37 @@ $(function () {
         dots:false,
         responsive:{
             0:{
-                items:1
+                items:2,
+                center:true
               
             },
-            600:{
+            767:{
                 items:3
                
             },
-            1000:{
-                items:5
-              
+            1023:{
+                items:5              
             }
         }
-    })
+    });
+
+    $('.work-process-list').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        dots:false,
+      items:1
+    });
+
+
+    // menu mobile
+$('.menu-open').click(function(){
+    $('.primary-menu').addClass('active');
+}); 
+$('.menu-close').click(function(){
+    $('.primary-menu').removeClass('active');
+}); 
+
 
 
 })
