e0e325aebde509702a8e06cb602bc54347bcace5
mukeshnegi1210 <33194021+mukeshnegi1210@users.noreply.github.com>
2020-09-03 17:02:13 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,42 +2,50 @@ $(function () {
 
 
     $('.partners-list').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        dots:false,
-        responsive:{
-            0:{
-                items:2,
-                center:true
-              
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 2,
+                center: true
+
             },
-            767:{
-                items:3
-               
+            767: {
+                items: 3
+
             },
-            1023:{
-                items:5              
+            1023: {
+                items: 5
             }
         }
     });
 
     $('.work-process-list').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        dots:false,
-      items:1
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        dots: false,
+        items: 1
     });
 
+    $('.banner-item-list').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        dots: false,
+        nav: true,
+        items: 1
+    });
 
     // menu mobile
-$('.menu-open').click(function(){
-    $('.primary-menu').addClass('active');
-}); 
-$('.menu-close').click(function(){
-    $('.primary-menu').removeClass('active');
-}); 
+    $('.menu-open').click(function () {
+        $('.primary-menu').addClass('active');
+    });
+    $('.menu-close').click(function () {
+        $('.primary-menu').removeClass('active');
+    });
 
 
 
