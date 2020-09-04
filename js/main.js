cc2cc2fdcd9321a7ca432cf6d1373b3c0df5d30d
mukeshnegi1210 <33194021+mukeshnegi1210@users.noreply.github.com>
2020-09-04 21:21:12 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -26,8 +26,9 @@ $(function () {
         loop: true,
         margin: 10,
         responsiveClass: true,
+        responsiveClass: true,
         dots: false,
-        items: 1
+        items: 1,
     });
 
     $('.banner-item-list').owlCarousel({
@@ -38,6 +39,25 @@ $(function () {
         nav: true,
         items: 1
     });
+    $('.problem-list').owlCarousel({
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        responsiveClass: true,
+        items: 3,
+        responsive: {
+            0: {
+                items: 1,
+                center: true
+
+            },
+            600: {
+                items: 2
+
+            },
+        }
+    });
 
     // menu mobile
     $('.menu-open').click(function () {
@@ -47,6 +67,11 @@ $(function () {
         $('.primary-menu').removeClass('active');
     });
 
+    // contact form button 
 
+    $('#banner-enquiry').click(function () {
+        $("html, body").animate({
+            scrollTop: $("#bottom-contact").offset().top - 50}, 600);
+    });
 
-})
+});
